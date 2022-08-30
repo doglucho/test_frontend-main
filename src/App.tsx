fe72117e5bd595c5b1467782d2a6ccfd7adaf723
Luciano Chirico <doglucho@MacBook-Air-de-Luciano.local>
2022-08-30 20:14:20 +0200

    function to connect with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { Component, useState } from 'react';
+import { render } from 'react-dom';
 import './App.scss';
 
 class App extends Component {
     checked?: boolean;
+    
+  // function App() {
+  //   const [data, setData] = React.useState(null);
+    
+  //   React.useEffect(() => {
+  //     fetch("/api")
+  //       .then((res) => res.json())
+  //       .then((data) => setData(data.message));
+  //   }, [])
+  // }
 
-    
   render() {
-    
     
     return (
       <div className="app">
